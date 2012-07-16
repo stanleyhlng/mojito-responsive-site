5ddec672421d6f8145ed8c4a0f5bb18f02590df6
Stanley Ng <stanleyhlng@yahoo.com.hk>
2012-07-16 14:44:32 +0800

    added lightbox element mojit

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,12 +2,30 @@
 
     var main = function() {
 
-        //jQuery(this).joyride({
-        //    "tipContent": "#page-guide-steps"
-        //});
+        /*
+        $(this).joyride({
+          "tipContent": "#page-guide-steps"
+        });
+        */
 
+        setupLightBox();
     };
-    
+
+    var setupLightBox = function() {
+        jQuery('#content .article .image-container').click(function(){
+            var display = $('#lightbox').css('display');
+            if (display === "block") {
+                $('#lightbox').css('display', 'none');
+            } else {
+                $('#lightbox').css('display', 'block');
+            }
+        });
+        
+        jQuery("#lightbox").click(function(){
+            $('#lightbox').css('display', 'none');
+        });
+    }
+
     jQuery( document ).ready( main );
     
 }(jQuery));
