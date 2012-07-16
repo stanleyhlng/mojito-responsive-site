3f362f426caf0463afa9039aa0b0f9ebc87777e2
Stanley Ng <stanleyhlng@yahoo.com.hk>
2012-07-16 17:20:46 +0800

    updated guide

--- assets/js/site.js
+++ assets/js/site.js
@@ -10,6 +10,7 @@
 
         setupLightBox();
         setupTour();
+        showHideGuides();
     };
 
     var setupLightBox = function() {
@@ -25,7 +26,7 @@
         jQuery("#lightbox").click(function(){
             $('#lightbox').css('display', 'none');
         });
-    }
+    };
 
     var setupTour = function() {
         jQuery("#banner .button").click(function(){
@@ -33,7 +34,23 @@
                 "tipContent": "#page-guide-steps"
             });
         });
-    }
+    };
+
+    var showGuide = true;
+    var showHideGuides = function() {
+        jQuery("#logo").click(function() {
+            if (showGuide) {
+                jQuery('.guide-page').css('border-color', 'rgba(0,0,255,1)');
+                jQuery('.guide-section').css('border-color', 'rgba(255,0,0,1)');
+                jQuery('.guide-element').css('border-color', 'rgba(255,0,255,1)');
+            } else {
+                jQuery('.guide-page').css('border-color', 'rgba(0,0,255,0)');
+                jQuery('.guide-section').css('border-color', 'rgba(255,0,0,0)');
+                jQuery('.guide-element').css('border-color', 'rgba(255,0,255,0)');
+            }
+            showGuide = !showGuide;
+        });
+    };
 
     jQuery( document ).ready( main );
     
